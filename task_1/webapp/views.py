d52ee483bf96e41f9d3a3f3d96749bbb17345fe7
tromm <trommxxx@mail.ru>
2021-02-15 19:55:26 +0600

    Added requirements.txt

---
+++ task_1/webapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'index.html')
+
+
+def article_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'article_create.html')
+    elif request.method == 'POST':
+        user = {
+            'name': 'One',
+            'mail': "one@one.ru"
+        }
+        context = {
+            'title': request.POST.get('title'),
+            'content': request.POST.get('content'),
+            'author': request.POST.get('author'),
+            'user': user
+        }
+
+        return render(request, 'article_view.html', context)
