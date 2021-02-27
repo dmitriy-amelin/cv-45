62b02574be1fbb86c7fea0a4add6d09a37f223f8
Reey <trommxxx@mail.ru>
2021-02-27 20:55:03 +0600

    Изменено создание статьи. Теперь при создании статьи создаётся экземпляр модели Article

--- task_1/webapp/views.py
+++ task_1/webapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from webapp.models import Article
+
 
 def index_view(request):
     return render(request, 'index.html')
@@ -9,14 +11,14 @@ def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        user = {
-            'name': 'One',
-            'mail': "one@one.ru"
-        }
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        author = request.POST.get("author")
 
-        return render(request, 'article_view.html', {
-            'title': request.POST.get("title"),
-            'content': request.POST.get("content"),
-            'author': request.POST.get("author"),
-            'user': user
-        })
+        article = Article.objects.create(
+            title=title,
+            content=content,
+            author=author
+        )
+
+        return render(request, 'home.html')
